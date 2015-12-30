c849750d128ab3171014167eecd6f43e974d8a7e
youai <youai@youais-Mac-mini.local>
2015-12-30 17:04:27 +0800

    change something

--- test.py
+++ test.py
@@ -52,6 +52,104 @@
 # person('Adam', 45, gender='M', job='Engineer')
 # person('a',34,**{'gender':2})
 
+# from testpy import man
+# man('Adam',age = 45)
 
+# from testpy import fact
+# from testpy import factx
+# n = int(input("number:"))
+# print(fact(n))
+# print(factx(n))
 
+# from testpy import moveF
+# move(16,'A','B','C')
 
+# from testpy import fbnc
+# n=int(input("number:"))
+# print(fbnc(n))
+
+#切片
+# ls = [1,2,3,4,]
+# print(ls[:3:2])
+
+#访问字典或者列表
+# dic = {'a':1,'b':2,'c':3}
+# ls = {1,2,3,4}
+# for k in dic:
+# 	print(k)
+# print('\n')
+
+# for v in dic.values():
+# 	print(v)
+# print('\n')
+
+# for k,v in dic.items():
+# 	print(k,v)
+
+# from collections import Iterable
+# a = isinstance('asd',Iterable)
+# print(a,'\n')
+
+# for i,value in enumerate(ls):
+# 	print(i,value)
+
+#列表生成式
+# ls = [x*x for x in range(10) if x%2==1]
+# ls = [x*y for x in range(10) if x%2==1 for y in range(10) if x%2==1]
+# import os
+# ls = [d for d in os.listdir('.')]
+# L1 = ['Hello', 'World', 18, 'Apple', None]
+# ls = [s.lower() for s in L1 if isinstance(s,str)]
+
+#九九乘法表
+# ls = [str(x) + ' x ' + str(y) + '=' + str(x*y) for x in range(1,10) for y in range(1,10)]
+# print(ls)
+# g = [x*x for x in range(10)]
+# for x in g:
+# 	print(x)
+
+# def fib(max):
+# 	n,a,b = 0,0,1
+# 	while n < max:
+# 		yield b
+# 		a,b = b,a + b
+# 		n = n + 1
+# 	return 'done'
+# g = fib(6)
+# while True:
+# 	try:
+# 		x = next(g)
+# 		print('g:',x)
+# 	except StopIteration as e:
+# 		print("return value:",e.value)
+# 		break
+
+#map and reduce
+# def mt(n):
+# 	return n * n
+# print(list(map(mt,[1,2,3])))
+
+# from functools import reduce
+# def fn(x,y):
+# 	return x *10+y
+# print(reduce(fn,[1,2,3,4,5,5,6,4,1]))	
+
+# from functools import reduce
+# def prod(L):
+# 	def mysum(x,y):
+# 		return x*y
+# 	return reduce(mysum,L)
+# print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+
+# from testpy import str2float
+# from testpy import primes
+
+def log(func):
+	def wrapper(*args,**kw):
+		print('call %s:'%func.__name__)
+		return func(*args,**kw)
+	return wrapper(*args,**kw)
+@log
+def now():
+	print("now")
+now()
--- testpy.py
+++ testpy.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def fa(x):
 	if not isinstance(x,(int ,float)):
 		raise TypeError('error')
@@ -13,6 +14,102 @@ def calc(*numbers):
 		sum = sum + n * n
 	return sum
 
-##**kw means dictionary
+##**kw means dictionary 
 def person(name,age,**kw):
 	print('name:', name, 'age:', age, 'other:', kw)
+
+#和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+def man(name, *,age):
+	print(name,age)
+
+#递归
+def fact(n):
+	if n == 1:
+		return 1
+	else:
+		return n * fact(n - 1)
+#递归会溢出,可以用尾递归优化
+def factx(n):
+	return fact_iter(n,1)
+def fact_iter(num,pro):
+	if num == 1:
+		return pro
+	return fact_iter(num - 1,num * pro)
+#汉诺塔问题
+
+def move(n,a,b,c):
+	if n==1:
+		print('move',a,'-->',c)
+		return
+	move(n-1,a,c,b)
+	print('move',a,'-->',c)
+	move(n-1,b,a,c)
+
+#斐波那契数列
+def fbnc(n):
+	if n==1 or n==0:
+		return n
+	return fbnc(n-1)+fbnc(n-2)
+
+#杨辉三角
+def triangles1():
+	b = [1]
+	i=0
+	while True:
+		yield b
+		b = [1] + [b[i] + b[i+1] for i in range(len(b)-1)] + [1]
+		print(b)
+
+def triangles2():
+    L=[1,]
+    while(True):
+        yield L 
+        i=len(L)-1
+        while(i):
+            L[i]=L[i]+L[i-1]
+            i-=1
+        L.append(1)
+
+# n=0
+# for t in triangles():
+#     print (t)
+#     n=n+1
+#     if n==10:
+#         break
+from functools import reduce
+def str2float(s):
+	def fn(x, y):
+		return x * 10 + y
+	def char2num(s):
+		return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[s]
+	return reduce(lambda x,y:x*10+y,map(char2num,s.replace('.','')))/(10**s[::-1].find('.') if s.find('.')!=-1 else 1)
+
+def _odd_iter():
+	n=1
+	while True:
+		n+=2
+		yield n
+
+def _not_divisible(n):
+	return lambda x:x%n>0
+
+def primes():
+	yield 2
+	it = _odd_iter()
+	while True:
+		n=next(it)
+		yield n
+		it = filter(_not_divisible,it)
+
+def by_name(t):
+	return t[0].lower()
+def by_score(t):
+	return t[1]
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+L2 = sorted(L, key=by_name)
+print(L2)
+L2 = sorted(L, key=by_score,reverse=True)
+print(L2)
+
+
+
