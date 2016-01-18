44373b2230d8210d130400ae07f1b443d35a7636
youai <youai@youais-Mac-mini.local>
2016-01-18 12:02:37 +0800

    help

--- test.py
+++ test.py
@@ -432,41 +432,42 @@
 # q = deque(['1','2'])
 
 #md5验证
-import hashlib
+# import hashlib
 
-def calc_md5(password):
-	md5 = hashlib.md5()
-	md5.update(password.encode('utf-8'))
-	return md5.hexdigest()
-db = {
-    'michael': 'e10adc3949ba59abbe56e057f20f883e',
-    'bob': '878ef96e86145580c38c87f0410ad153',
-    'alice': '99b1c2188db85afee403b1536010c2c9'
-}
+# def calc_md5(password):
+# 	md5 = hashlib.md5()
+# 	md5.update(password.encode('utf-8'))
+# 	return md5.hexdigest()
+# db = {
+#     'michael': 'e10adc3949ba59abbe56e057f20f883e',
+#     'bob': '878ef96e86145580c38c87f0410ad153',
+#     'alice': '99b1c2188db85afee403b1536010c2c9'
+# }
 
-def login(user,password):
-	md5 = hashlib.md5()
-	md5.update(password.encode('utf-8'))
-	print(md5.hexdigest())
-	if db[user] == md5.hexdigest():
-		return True
-	return False
-def get_md5(token):
-	md5 = hashlib.md5()
-	md5.update(token.encode('utf-8'))
-	return md5.hexdigest()
+# def login(user,password):
+# 	md5 = hashlib.md5()
+# 	md5.update(password.encode('utf-8'))
+# 	print(md5.hexdigest())
+# 	if db[user] == md5.hexdigest():
+# 		return True
+# 	return False
+# def get_md5(token):
+# 	md5 = hashlib.md5()
+# 	md5.update(token.encode('utf-8'))
+# 	return md5.hexdigest()
 
-dbs = {}
-def register(user,password):
-	db[user] = get_md5(password + user + 'the-Salt')
+# dbs = {}
+# def register(user,password):
+# 	db[user] = get_md5(password + user + 'the-Salt')
 
-def log_in(user,password):
-	md5 = hashlib.md5()
-	token = password + user + 'the-Salt'
-	md5.update(token.encode('utf-8'))
-	if db[user] == md5.hexdigest():
-		return True
-	return False
-register('michael','123456')
-print(log_in('michael','123456'))
+# def log_in(user,password):
+# 	md5 = hashlib.md5()
+# 	token = password + user + 'the-Salt'
+# 	md5.update(token.encode('utf-8'))
+# 	if db[user] == md5.hexdigest():
+# 		return True
+# 	return False
+# register('michael','123456')
+# print(log_in('michael','123456'))
 
+#test git sublime text
