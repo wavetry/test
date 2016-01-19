9fb8231317adc3792ccc76132f5561879508dd25
youai <youai@youais-Mac-mini.local>
2016-01-19 14:32:08 +0800

    tcp udp

--- test.py
+++ test.py
@@ -470,4 +470,57 @@
 # register('michael','123456')
 # print(log_in('michael','123456'))
 
-#test git sublime text
+#client
+# import socket
+
+# s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# s.connect(('www.sina.com.cn',80))
+# s.send(b'GET / HTTP/1.1\r\nHost: www.sina.com.cn\r\nConnection: close\r\n\r\n')
+
+# buffer = []
+# while True:
+# 	d = s.recv(1024)
+# 	if d:
+# 		buffer.append(d)
+# 	else:
+# 		break
+# data = b''.join(buffer)
+# s.close()
+# header, html = data.split(b'\r\n\r\n', 1)
+# print(header.decode('utf-8'))
+# # 把接收的数据写入文件:
+# with open('sina.html', 'wb') as f:
+#     f.write(html)
+
+
+#server
+# import socket
+# import time, threading
+# s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# s.bind(('127.0.0.1',9999))
+# s.listen(5)
+# print("Waiting for connection...")
+# def tcplink(sock,addr):
+# 	print("Accept new connection from %s:%s..."%addr)
+# 	sock.send(b'welcome')
+# 	while True:
+# 		data  = sock.recv(1024)
+# 		time.sleep(1)
+# 		if not data or data.decode('utf-8') == 'exit':
+# 			break
+# 		sock.send(('Helo,%s' % data.decode('utf-8')).encode('utf-8'))
+# 	sock.close()
+# 	print('Connection from %s:%s closed.' % addr)
+
+# while True:
+# 	sock,addr = s.accept()
+# 	t = threading.Thread(target = tcplink,args = (sock,addr))
+# 	t.start()
+
+#client udp
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+for data in [b'Michael', b'Tracy', b'Sarah']:
+	s.sendto(data,('127.0.0.1',9999))
+	print(s.recv(1024).decode('utf-8'))
+s.close()
